f546d613aeff7fb77b028ffdd44d34ba86a1fb5f
root <root@STERF.localdomain>
2021-01-03 10:56:56 +0100

    add project files

---
+++ script.js
@@ -0,0 +1,9 @@
+function show(){
+    let back = document.getElementById('back');
+    if( back.style.display === "flex"){
+        back.style.display = "none";
+    }
+    else{
+        back.style.display = "flex";
+    }
+}
